fde11604eeea4f9c113ef6582681f0dc9722b9e7
Johan Svensson <johan.svensson@strongpoint.com>
2024-12-03 11:52:15 +0100

    Day 2 solution

---
+++ AdventOfCode2024/Day2.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2024;
+
+internal class Day2
+{
+    private static IEnumerable<IEnumerable<int>> Reports()
+    {
+        return File.ReadAllLines(@"..\..\..\Day2Input.txt").Select(line => line.Split(" ").Select(value => int.Parse(value)));
+    }
+
+    private static bool IsReportSafe(IEnumerable<int> report, bool useProblemDampener = false)
+    {
+        //Attempt to solve the problem without the dampener
+        var increasing = report.First() < report.Skip(1).First();
+        var isSafeReport = report.SkipLast(1).Zip(report.Skip(1), (a, b) => (increasing ? a < b : a > b) && (Math.Abs(a - b) is >= 1 and <= 3)).All(isSafe => isSafe);
+
+        //If the dampener is enabled and the report is not safe, apply dampener and try again
+        if (!isSafeReport && useProblemDampener)
+        {
+            var removeIndex = 0;
+            while (removeIndex < report.Count() && !isSafeReport)
+            {
+                var newReport = report.Where((value, index) => index != removeIndex);
+                increasing = newReport.First() < newReport.Skip(1).First();
+                isSafeReport = newReport.SkipLast(1).Zip(newReport.Skip(1), (a, b) => (increasing ? a < b : a > b) && (Math.Abs(a - b) is >= 1 and <= 3)).All(isSafe => isSafe);
+                removeIndex++;
+            }
+        }
+
+        //Return status if report is safe
+        return isSafeReport;
+    }
+
+    public static int ExecutePart1() => Reports().Where(report => IsReportSafe(report)).Count();
+
+    public static int ExecutePart2() => Reports().Where(report => IsReportSafe(report, true)).Count();
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -7,5 +7,9 @@ internal class Program
         //Day 1
         Console.WriteLine($"Result Day 1 (Part 1): {Day1.ExecutePart1()}");
         Console.WriteLine($"Result Day 1 (Part 2): {Day1.ExecutePart2()}");
+
+        //Day 2
+        Console.WriteLine($"Result Day 2 (Part 1): {Day2.ExecutePart1()}");
+        Console.WriteLine($"Result Day 2 (Part 2): {Day2.ExecutePart2()}");
     }
 }
